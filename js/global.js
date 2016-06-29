5f3212f610474bfe7f0a0968a512951fd7921d94
UE <520UED.com@gmail.com>
2016-06-29 14:16:56 +0800

    提交
    提交

--- js/global.js
+++ js/global.js
@@ -9,3 +9,30 @@ $("body").on('touchStart',function(){})
 $("header .btn").click(function(){
     $(".drop-box").slideToggle();
 });
+
+
+
+
+
+// pure JS
+var elem = document.getElementById('mySwipe');
+window.mySwipe = Swipe(elem, {
+    startSlide: 4,
+    auto: 3000,
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: function(index, element) {
+//     console.log(mySwipe.slide(1, 100))
+
+    },
+    transitionEnd: function(index, element) {
+        console.log("go2")
+    }
+});
+
+
+
+
+
+
