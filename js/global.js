67837cba68b2c35771a3e88684ee1e3f02939999
UE <520UED.com@gmail.com>
2016-07-02 18:47:58 +0800

    所有页面
    所有页面

--- js/global.js
+++ js/global.js
@@ -2,6 +2,21 @@
  * Created by owenhong on 2016/6/26.
  */
 
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 75 * (clientWidth / 375) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
+
+
 $("body").on('touchStart',function(){})
 
 
@@ -44,6 +59,91 @@ window.mySwipe = Swipe(elem, {
     transitionEnd: function(index, element) {
     }
 });
+
+
+
+
+$(".select-color li").click(function(){
+    var $img = $(this).data('img');
+    $(this).addClass('cur').siblings().removeClass('cur');
+
+    $(".car-img img").attr("src",$img);
+
+})
+
+// pure JS
+var elem = document.getElementById('mySwipe2');
+window.mySwipe = Swipe(elem, {
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: function(index, element) {
+        $("#swipe-list1 li").eq(index).addClass('cur').siblings().removeClass('cur')
+
+        //var $this_height = $(".core-box").eq(index).height();
+        //
+        //$(".swipe-wrap").css('height',$this_height);
+    },
+    transitionEnd: function(index, element) {
+    }
+});
+// pure JS
+var elem = document.getElementById('mySwipe3');
+window.mySwipe = Swipe(elem, {
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: function(index, element) {
+        $("#swipe-list2 li").eq(index).addClass('cur').siblings().removeClass('cur')
+
+        //var $this_height = $(".core-box").eq(index).height();
+        //
+        //$(".swipe-wrap").css('height',$this_height);
+    },
+    transitionEnd: function(index, element) {
+    }
+});
+// pure JS
+var elem = document.getElementById('mySwipe4');
+window.mySwipe = Swipe(elem, {
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: function(index, element) {
+        $("#swipe-list4 li").eq(index).addClass('cur').siblings().removeClass('cur')
+
+        //var $this_height = $(".core-box").eq(index).height();
+        //
+        //$(".swipe-wrap").css('height',$this_height);
+    },
+    transitionEnd: function(index, element) {
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
